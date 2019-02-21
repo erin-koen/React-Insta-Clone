a5a3161cafd6379271b083acd10e56293c8cfe83
erin-koen <erin.koen@gmail.com>
2019-02-21 11:21:30 -0500

    cleaning changes from last night

--- instagram/src/Components/Post Header/postHeader.js
+++ instagram/src/Components/Post Header/postHeader.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './postHeader.css';
+import styled from 'styled-components';
+
+const StyledPostHeader = styled.div`
+    display: flex;
+    flex-direction: row;    
+    align-items: center;
+
+    img {
+        border-radius: 50%;
+        width: 48px;
+        height: 48px;
+        margin: 10px;
+    }
+`
+
 
 function PostHeader(props){
     return(
-        <div className='header'>
-            <img src={`${props.post.thumbnailUrl}`} alt=''/>
+        <StyledPostHeader>
+            <img src={`${props.post.thumbnailUrl}`} alt=''></img> 
             <h4>{props.post.username}</h4>
-            
-        </div>
+        </StyledPostHeader>
     )
 }
 
--- instagram/src/Components/PostConstainer/PostContainer.js
+++ instagram/src/Components/PostConstainer/PostContainer.js
@@ -2,17 +2,20 @@ import React from 'react';
 import CommentSection from '../CommentSection/CommentSection';
 import './PostContainer.css';
 import Photo from '../Photo/photo';
-import PostHeader from '../Post Header/postHeader'
-import PostLikes from '../Post Likes/PostLikes'
+import PostHeader from '../Post Header/postHeader';
+import PostLikes from '../Post Likes/PostLikes';
+import styled from 'styled-components';
+
+
 
 
 function PostContainer(props){
     return(
         <div className='post-container'>
-            <div className='post-header'>
-                <PostHeader post={props.post} />
+            <div>
+                <PostHeader classname='post-header' post={props.post} />
             </div>
-
+                
             <div className='post-photo'>
                 <Photo post={props.post} />
             </div>
