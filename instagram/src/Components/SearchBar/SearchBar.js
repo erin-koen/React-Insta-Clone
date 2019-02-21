e73165b383553c689cb9194b79e6b614a5e4f810
erin-koen <erin.koen@gmail.com>
2019-02-21 16:01:18 -0500

    search bar styled-components-style

--- instagram/src/Components/SearchBar/SearchBar.js
+++ instagram/src/Components/SearchBar/SearchBar.js
@@ -1,32 +1,81 @@
 import React from 'react';
 import './SearchBar.css';
-import logo from './instagram-1.svg';
+import logo from './iglogo.png';
+import styled from 'styled-components';
+
+const SearchHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+    height: 75px;
+    align-items: center;
+`;
+ 
+const InstaLogoContainer = styled.div`
+    max-width: 200px;
+    display: flex;
+    flex-direction: row;
+    // justify-content: center;    
+    align-items: flex-start;
+    margin-left: 20px;
+
+    img {
+        width: 35%
+        height: 35%;
+        padding-left: 10px;
+    }
+    div {
+        border-left: 1px solid grey;
+        border-right: 1px solid grey;
+    }
+    i {
+        width: 24px;
+        height: 24px;
+        padding-left: 10px;
+    }
+    `
+const SearchForm = styled.form`
+    // margin-right: 50px;
+`
+
+const ProfileLinks = styled.div`
+    margin-right: 30px;;
+
+    i {
+        margin-left:15px;
+    }
+`    
 
 
 const SearchBar = (props) => {
 
     return (
-        <div className="search-bar-header">
-            <div className="insta-logos">
+        <SearchHeader>
+            <InstaLogoContainer>
                 <i className='fab fa-instagram' />
-                <div>|</div>
+                <div><p></p></div>
                 <img className="insta-font-icon" src={logo} alt=''/>
-            </div>
-            <form onChange={props.filterResults} onSubmit={props.filterResults}>
+            </InstaLogoContainer>
+            <SearchForm onChange={props.filterResults} onSubmit={props.filterResults}>
                 <input 
-                type="text" 
-                placeholder="SEARCH BY USERNAME"
-                name="filterElement"
-                value={props.filterElement}
-                onChange={props.handleChanges}
-                 />
-            </form>
-            <div className="profile-links">
-                <i className='fa fa-compass' />
-                <i className='fa fa-heart' />
-                <i className='fa fa-user-circle' />
-            </div>
-        </div>
+                    type="text" 
+                    placeholder="SEARCH BY USERNAME"
+                    name="filterElement"
+                    value={props.filterElement}
+                    onChange={props.handleChanges}
+                />
+            </SearchForm>
+            <ProfileLinks>
+                <i className='far fa-compass' />
+                <i className='far fa-heart' />
+                <i className='far fa-user' />
+            </ProfileLinks>
+            
+        </SearchHeader>
+
+            
+
     )
 }
 
